e40e4c62ed553a1aa83895edc6db2e13daf15e1c
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-04-25 14:45:14 +0300

    Added source code comments.

--- Dotnet/PredictNumber/Program.cs
+++ Dotnet/PredictNumber/Program.cs
@@ -2,13 +2,18 @@
 {
     private static void Main(string[] args)
     {
+        // select the randoom number
         var random = new Random();
         var number = random.Next(1,20);
         Console.WriteLine(number);
+
+        // player has 3 attempts
         for(var i = 0; i < 3; i++) 
         {
             Console.Write(string.Format("Prediction Attempt {0}: ", i + 1));
             var input = int.Parse(Console.ReadLine());
+
+            // check the guess
             if(number == input)
             {
                 Console.WriteLine("You Won!");
