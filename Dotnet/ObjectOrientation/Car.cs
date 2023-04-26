81d3ffdfd1812193ddf0958b8b3d0832998ec0c9
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-04-26 11:59:15 +0300

    Added error handelling to acceleration and deceleration methods.

--- Dotnet/ObjectOrientation/Car.cs
+++ Dotnet/ObjectOrientation/Car.cs
@@ -1,9 +1,41 @@
 public class Car
 {
-
+    // properties
     public String Model { get; set; }
     public String Make { get; set; }
-    public int CurrentSpeed { get; }
-    public bool EnginRunning { get; }
+    public int CurrentSpeed { get; private set; }
+    public bool EnginRunning { get; private set; }
+
+    public void StartEngin()
+    {
+        EnginRunning = true;
+    }
+
+    public void StopEngin()
+    {
+        EnginRunning = false;
+    }
+
+    public void Accelerate()
+    {
+        if ( !EnginRunning )
+        {
+            throw new Exception( "Cannot accelerate because the engin is not running. Please use StartEngin() first.");
+        }
+        if ( CurrentSpeed > 200)
+        {
+            CurrentSpeed = 200;
+        }
+        CurrentSpeed += 10;
+    }
+
+    public void Decelerate()
+    {
+        if ( CurrentSpeed < 0)
+        {
+            CurrentSpeed = 0;
+        }
+        CurrentSpeed -= 10;
+    }
     
 }
--- Dotnet/ObjectOrientation/Program.cs
+++ Dotnet/ObjectOrientation/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 Car myCar = new();
 myCar.Make = "Toyota";
+myCar.Model = "Vitz";
+myCar.StartEngin();
+myCar.Accelerate();
+Console.WriteLine("Current speed of the car is: " + myCar.CurrentSpeed);
 
 
