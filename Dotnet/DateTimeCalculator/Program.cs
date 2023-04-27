c558fbf8ba5dca58e880a3774ce739684ca7c756
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-04-27 11:15:25 +0300

    Addin initial implimentation of converter

--- Dotnet/DateTimeCalculator/Program.cs
+++ Dotnet/DateTimeCalculator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(@"Enter the starting date and time in following format 'DD-MM-YYYY HH:MM:SS'");
+var startingDateString = Console.ReadLine();
+DateTime startingDateTime;
+DateTime endDateTime;
+if (DateTime.TryParse(startingDateString, out startingDateTime))
+{
+    startingDateTime = startingDateTime;
+}
+else
+{
+    Console.WriteLine("Invalid date and time. Please enter date and time in correct format");
+}
+Console.WriteLine();
+Console.WriteLine(@"Enter the end date and time in following format 'DD-MM-YYYY HH:MM:SS'");
+var endDateString = Console.ReadLine();
+if (DateTime.TryParse(endDateString, out endDateTime))
+{
+    endDateTime = endDateTime;
+}
+else
+{
+    Console.WriteLine("Invalid date and time. Please enter date and time in correct format");
+}
+var duration = endDateTime - startingDateTime;
+Console.WriteLine(duration.TotalDays.ToString());
+
