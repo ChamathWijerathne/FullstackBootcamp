3e23d67ce7080cd277aebc6648d1281ef5030abd
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-04-27 10:29:39 +0300

    Adding File reader program

---
+++ Dotnet/FileReader/Program.cs
@@ -0,0 +1,14 @@
+﻿StreamReader reader = new StreamReader("file.txt");
+
+// Read each line of the file and calculate the total
+string line;
+int count = 0;
+while ((line = reader.ReadLine()) != null)
+{
+    count += int.Parse(line);
+}
+
+// Close the file
+reader.Close();
+
+Console.WriteLine(count);
