21d664e797630ad1456d01cf218c42f9af556b66
Chamath Wijerathne <anuradhika.wije@gmail.com>
2023-05-04 15:49:05 +0300

    Created new project to display random numbers grater than 20.

---
+++ Dotnet/Randoms/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] myArray = new int[15];
+Random random = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = random.Next(50);
+}
+
+foreach(int number in myArray)
+{
+    Console.WriteLine(number);
+}
